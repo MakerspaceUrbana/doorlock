e0ccb7bc26ec4886ec6c0db4e5e56e2572306908
brian <brn.j.williams@gmail.com>
2013-01-29 20:58:47 -0600

    adding arduion CLI makefile

--- arduino/ReadWrite/ReadWrite.ino
+++ arduino/ReadWrite/ReadWrite.ino
@@ -16,7 +16,7 @@
  
  This example code is in the public domain.
  	 
- */
+*/
  
 #include <SD.h>
 
@@ -24,11 +24,9 @@ File myFile;
 
 void setup()
 {
- // Open serial communications and wait for port to open:
+  
+  // Open serial communications and wait for port to open:
   Serial.begin(9600);
-   while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
 
 
   Serial.print("Initializing SD card...");

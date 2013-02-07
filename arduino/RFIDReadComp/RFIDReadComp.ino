6bca2532eac5b98d2c9a51d0d8c984f10a197a3a
brian <brn.j.williams@gmail.com>
2013-02-06 20:41:27 -0600

    wont fail

---
+++ arduino/RFIDReadComp/RFIDReadComp.ino
@@ -0,0 +1,95 @@
+
+// RFID reader for Arduino 
+// Wiring version by BARRAGAN <http://people.interaction-ivrea.it/h.barragan> 
+// Modified for Arudino by djmatic
+
+
+int  val = 0; 
+char code[11]; 
+int bytesread = 0; 
+
+#include <SD.h>
+
+File myFile;
+
+void setup() { 
+
+Serial.begin(2400); // RFID reader SOUT pin connected to Serial RX pin at 2400bps 
+
+Serial.print("Initializing SD card...");
+pinMode(10, OUTPUT);
+
+if (!SD.begin(4)) {
+    Serial.println("initialization failed!");
+    return;
+    }
+Serial.println("Initialization done.");
+
+pinMode(2,OUTPUT);   // Set digital pin 2 as OUTPUT to connect it to the RFID /ENABLE pin 
+digitalWrite(2, LOW);                  // Activate the RFID reader
+}  
+
+ void loop() { 
+
+  if(Serial.available() > 0) {          // if data available from reader 
+    if((val = Serial.read()) == 10) {   // check for header 
+      bytesread = 0; 
+      while(bytesread<10) {              // read 10 digit code 
+        if( Serial.available() > 0) { 
+          val = Serial.read(); 
+          if((val == 10)||(val == 13)) { // if header or stop bytes before the 10 digit reading 
+            break;                       // stop reading 
+          } 
+          code[bytesread] = val;         // add the digit           
+          bytesread++;                   // ready to read next digit  
+        } 
+      } 
+      code[10]='\0';
+      
+      if(bytesread == 10) {              // if 10 digit read is complete 
+        Serial.print("TAG code is: ");   // possibly a good TAG 
+        Serial.println(code);            // print the TAG code 
+	
+	char fbuffer[20];
+	int foo;
+	char mycmp[] = "0600966908";
+	myFile = SD.open("KEYLIST.TXT");
+	if (myFile) {
+	   Serial.println("KEYLIST.TXT:");
+	   
+	   while (myFile.available()){
+	     foo = myFile.readBytesUntil('\n', fbuffer, 20);
+	     fbuffer[foo] = '\0';
+	     Serial.write("Bytes read: ");
+	     Serial.write(fbuffer);
+
+	     if (strncmp(code, fbuffer, 10) == 0 ) {
+	         Serial.write("  THE ONE!!!!\r\n");
+		 break;
+	     }
+	     else {
+	     	  Serial.write("  You lose");
+	     }
+	     Serial.write("\r\n");
+	   }
+        }
+	else {
+	     Serial.println("error opening file");
+	}
+	
+	// close the file:
+	myFile.close();
+      }
+
+      
+    
+      bytesread = 0; 
+      digitalWrite(2, HIGH);                  // deactivate the RFID reader for a moment so it will not flood
+           delay(1500);                       // wait for a bit 
+           digitalWrite(2, LOW);                  // Activate the RFID reader
+    } 
+  } 
+} 
+
+// extra stuff
+// digitalWrite(2, HIGH);             // deactivate RFID reader 0
---
+++ arduino/backup/readWriteBackup/build-cli/ReadWrite.cpp
@@ -0,0 +1,95 @@
+#include <Arduino.h>
+/*
+  SD card read/write
+ 
+ This example shows how to read and write data to and from an SD card file 	
+ The circuit:
+ * SD card attached to SPI bus as follows:
+ ** MOSI - pin 11
+ ** MISO - pin 12
+ ** CLK - pin 13
+ ** CS - pin 4
+ 
+ created   Nov 2010
+ by David A. Mellis
+ modified 9 Apr 2012
+ by Tom Igoe
+ 
+ This example code is in the public domain.
+ 	 
+*/
+ 
+#include <SD.h>
+
+File myFile;
+
+void setup()
+{
+  
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+
+
+  Serial.print("Initializing SD card...");
+  // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
+  // Note that even if it's not used as the CS pin, the hardware SS pin 
+  // (10 on most Arduino boards, 53 on the Mega) must be left as an output 
+  // or the SD library functions will not work. 
+   pinMode(10, OUTPUT);
+   
+  if (!SD.begin(4)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
+  
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  myFile = SD.open("test.txt", FILE_WRITE);
+  
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.print("Writing to test.txt...");
+    myFile.println("testing 1, 2, 3.");
+	// close the file:
+    myFile.close();
+    Serial.println("done.");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+  
+  char fbuffer[20];
+  int foo;
+  // re-open the file for reading:
+  myFile = SD.open("KEYLIST.TXT");
+  if (myFile) {
+    Serial.println("KEYLIST.TXT:");
+    
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+        foo = myFile.readBytesUntil('\n', fbuffer, 20);
+        String output = "Bytes read: ";
+        output += String(foo, DEC);
+        char output2[30];
+        output.toCharArray(output2, 0, 30);
+        Serial.write(output2);
+        Serial.write("\n");
+    	Serial.write(fbuffer);
+        Serial.write("read done\n");
+        
+    }
+    // close the file:
+    myFile.close();
+  } else {
+  	// if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+}
+
+void loop()
+{
+	// nothing happens after setup
+}
+
+
